cff819be5f5c1ff29e326333b0b788d30ea832e9
Martyn Janes <martyn@obany.com>
2018-10-29 09:53:07 +0300

    Expanded output log

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ export BUILD_OUTPUT_TMP=$WORKDIR/build.tmp
 touch $BUILD_OUTPUT
 
 dump_output() {
-   echo Tailing the last 500 lines of output:
-   tail -500 $BUILD_OUTPUT
+   echo Tailing the last 1000 lines of output:
+   tail -1000 $BUILD_OUTPUT
 }
 error_handler() {
   echo ERROR: An error was encountered with the build.
